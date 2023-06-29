f301e2af176b5ef343cb8bff6c67cf1b544418de
aturulin <Aleksandr.Turulin@kauri-iot.com>
2023-06-29 11:27:12 +0300

    Добавлено чтение списка предметов из файла и сохранение файла output.

--- src/main/java/com/turulin/Main.java
+++ src/main/java/com/turulin/Main.java
@@ -1,30 +1,51 @@
 package com.turulin;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String args) {
+    private static String pathToInputFile = "/home/aturulin/IdeaProjects/SintecaTestTask/src/main/resources/input.txt";
+    private static String pathToOutputFile = "/home/aturulin/IdeaProjects/SintecaTestTask/src/main/resources/output.txt";
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
+        findSimilar("input.txt");
     }
 
-    public static String findSimilar(String inputFileName) throws URISyntaxException {
+    public static String findSimilar(String inputFileName) throws Exception {
 
-        return Paths.get(Main.class.getClassLoader().getResource("output.txt").toURI()).toString();
+        //возможно надо указать ресурсную папку
+        var firstItemList = new ArrayList<String>();
+        var secondItemList = new ArrayList<String>();
+        try (var bufferedReader = new BufferedReader(new FileReader(pathToInputFile))) {
+            var sizeFirstItemList = Integer.parseInt(bufferedReader.readLine());
+            for (int i = 0; i < sizeFirstItemList; ++i) {
+                firstItemList.add(bufferedReader.readLine());
+            }
+            int sizeSecondItemList = Integer.parseInt(bufferedReader.readLine());
+            for (int i = 0; i < sizeSecondItemList; ++i) {
+                secondItemList.add(bufferedReader.readLine());
+            }
+        }
+
+        //var mapFirstSecondItemListMatching = getMapOfSimple();
+
+        return saveMapToFile(new HashMap<>()).toString();
     }
 
     public static Map<String, String> getMapOfSimple(String filePath) throws Exception {
         return new HashMap<String, String>();
     }
 
-    public static void saveMapToFile(Map<String, String> results) throws Exception {
-        Path pathToResultFile = Paths.get(Main.class.getClassLoader().getResource("output.txt").toURI());
+    public static Path saveMapToFile(Map<String, String> results) throws Exception {
+        Path pathToResultFile = Path.of(pathToOutputFile);
         if (Files.notExists(pathToResultFile))
             Files.createFile(pathToResultFile);
 
@@ -33,7 +54,8 @@ public class Main {
                 bufferedWriter.write(entry.toString());
                 bufferedWriter.write("\n");
             }
-
         }
+
+        return pathToResultFile;
     }
 }
